742417f838e54b9f6d85d21719ceae0bb7ca0852
Saptak <i.am.saptakchaki@gmail.com>
2025-09-19 19:16:29 +0530

    worker-portal improved

--- src/pages/WorkerTasks.jsx
+++ src/pages/WorkerTasks.jsx
@@ -807,7 +807,7 @@ const WorkerTasks = () => {
         </div>
 
         {/* Quick Action Buttons */}
-        <motion.section 
+        {/* <motion.section 
           initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
           transition={{ duration: 0.5, delay: 0.6 }}
@@ -831,7 +831,7 @@ const WorkerTasks = () => {
               Helpdesk
             </button>
           </div>
-        </motion.section>
+        </motion.section> */}
       </div>
 
       {/* Shift Summary Modal */}
