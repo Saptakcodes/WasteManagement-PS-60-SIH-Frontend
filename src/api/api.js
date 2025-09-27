f3cf0d9ce0eba9fef940d692dcf78c6d2087ef0f
SANDIPAN JHA <sandipanjha3@gmail.com>
2025-09-27 23:23:49 +0530

    Fix: origin changed

--- src/api/api.js
+++ src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-   baseURL: "http://localhost:5000/api/v1",
-  // baseURL: import.meta.env.VITE_BACKEND_URL,
+  //baseURL: "http://localhost:5000/api/v1",
+  baseURL: import.meta.env.VITE_BACKEND_URL,
   withCredentials: true,
 });
 
